068f3eb9e30d0fdb2775dfa98dcbc0d00bf12afa
Stepbackru <alex-ruba@bk.ru>
2021-05-30 00:44:19 +0300

    feat: add class Main and render main template

---
+++ src/pages/index.ts
@@ -0,0 +1,26 @@
+import ObjInterface from '../assets/interfaces/interfaceRender';
+import elem from '../assets/utils/createElement';
+import mainTemplate from '../assets/data/mainTemplate';
+import aboutPage from './about/about';
+
+export default class Main {
+  private elements: HTMLElement[] = [];
+
+  public render(): void {
+    this.createTemplate(mainTemplate);
+    aboutPage.render();
+    console.log(this.elements);
+  }
+
+  // subscribe() {
+
+  // }
+
+  private createTemplate(arr: ObjInterface[]): void {
+    arr.forEach((elems: ObjInterface) => {
+      elem.add(elems);
+    });
+    this.elements = elem.getElements();
+    elem.clearElements();
+  }
+}
