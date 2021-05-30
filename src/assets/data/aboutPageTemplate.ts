635f09532db8325f1cdcda7f470a27afb39333a9
Stepbackru <alex-ruba@bk.ru>
2021-05-30 16:40:52 +0300

    feat: add structure page to aboutPageTemplate

--- src/assets/data/aboutPageTemplate.ts
+++ src/assets/data/aboutPageTemplate.ts
@@ -1,8 +1,94 @@
+/* eslint-disable max-len */
 const aboutPageTemplate = [
   {
     el: 'section',
-    classNames: 'test',
-    parent: 'body',
+    classNames: 'about',
+    parent: 'main > .wrapper',
+    child: [
+      {
+        el: 'h2',
+        classNames: 'about__caption',
+        text: 'How to play?',
+      },
+      {
+        el: 'div',
+        classNames: 'about__item',
+        child: [
+          {
+            el: 'div',
+            classNames: 'about__description',
+            child: [
+              {
+                el: 'p',
+                classNames: 'about__number',
+                text: '1',
+              },
+              {
+                el: 'p',
+                classNames: 'about__info',
+                text: 'Register new player in game',
+              },
+            ],
+          },
+          {
+            el: 'div',
+            classNames: 'about__example about__example-reg',
+          },
+        ],
+      },
+      {
+        el: 'div',
+        classNames: 'about__item',
+        child: [
+          {
+            el: 'div',
+            classNames: 'about__description',
+            child: [
+              {
+                el: 'p',
+                classNames: 'about__number',
+                text: '2',
+              },
+              {
+                el: 'p',
+                classNames: 'about__info',
+                text: 'Configure your game settings',
+              },
+            ],
+          },
+          {
+            el: 'div',
+            classNames: 'about__example about__example-settings',
+          },
+        ],
+      },
+      {
+        el: 'div',
+        classNames: 'about__item',
+        child: [
+          {
+            el: 'div',
+            classNames: 'about__description',
+            child: [
+              {
+                el: 'p',
+                classNames: 'about__number',
+                text: '3',
+              },
+              {
+                el: 'p',
+                classNames: 'about__info',
+                text: 'Start you new game! Remember card positions and match it before times up.',
+              },
+            ],
+          },
+          {
+            el: 'div',
+            classNames: 'about__example about__example-start',
+          },
+        ],
+      },
+    ],
   },
 ];
 
